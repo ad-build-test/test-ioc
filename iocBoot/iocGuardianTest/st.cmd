f14307fee07bf5530cb36898de170dc1cc18b117
pnispero <pnispero@slac.stanford.edu>
2024-06-17 13:56:56 -0700

    Added example ioc with asyn support

---
+++ iocBoot/iocGuardianTest/st.cmd
@@ -0,0 +1,30 @@
+#!../../bin/linux-x86_64/Guardian
+
+#- You may have to change Guardian to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/Guardian.dbd"
+Guardian_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+#dbLoadRecords("db/xxx.db","user=GUARDIAN")
+dbLoadRecords("db/test.db") # PATRICK TODO: Temp here for testing
+dbLoadRecords("db/guardian_snapshot.db", "BASE=SIOC:B34:GD_PATRICK") # PATRICK TODO: Temp add patrick so its unique
+dbLoadRecords("db/guardian_device_data.db", "BASE=SIOC:B34:GD_PATRICK") # PATRICK TODO: Temp add patrick so its unique
+
+
+## Configure Guardian driver
+# GuardianDriverConfigure(
+#    Port Name,                 # Name given to this port driver
+GuardianDriverConfigure("GUARDIAN")
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=GUARDIAN"
