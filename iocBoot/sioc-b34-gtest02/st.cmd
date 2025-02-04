03c58ff7b8448d8d3267f643ce2cbc30f1183536
pnispero <pnispero@slac.stanford.edu>
2025-02-04 10:08:09 -0800

    Addeed 2 mock iocs for deployment testing

---
+++ iocBoot/sioc-b34-gtest02/st.cmd
@@ -0,0 +1,31 @@
+#!../../bin/rhel9-x86_64/Guardian
+
+#- You may have to change Guardian to something else
+#- everywhere it appears in this file
+#- Same content as sioc-b34-gtest01, but this is sioc-b34-gtest02. Both used for deployment testing purposes
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/Guardian.dbd"
+Guardian_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+#dbLoadRecords("db/xxx.db","user=GUARDIAN")
+dbLoadRecords("db/test.db") # PATRICK TODO: Temp here for testing
+dbLoadRecords("db/guardian_snapshot.db", "BASE=SIOC:B34:GD_PATRICK") # PATRICK TODO: Temp add patrick so its unique
+dbLoadRecords("db/guardian_device_data.db", "BASE=SIOC:B34:GD_PATRICK") # PATRICK TODO: Temp add patrick so its unique
+
+
+## Configure Guardian driver
+# GuardianDriverConfigure(
+#    Port Name,                 # Name given to this port driver
+GuardianDriverConfigure("GUARDIAN")
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=GUARDIAN"
